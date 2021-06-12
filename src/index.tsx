24d40265215bc6c5d1c4bb534dc291f3ab95d430
Antoni Papiewski <antoni.papiewski@dac.digital>
2021-06-12 21:01:39 +0200

    add 'select' element ; minor refacttor

--- src/index.tsx
+++ src/index.tsx
@@ -1,105 +1,103 @@
 import React, { HTMLProps } from "react";
 import classnames from "classnames";
 
-
 type TClasserElProps<TagType, Additional> = HTMLProps<TagType> & Additional;
 
-type TClasserResolver<TagType, Additional> = (
-    TemplateStringsArray |
-    string |
-    ((elProps: TClasserElProps<TagType, Additional>) => Parameters<typeof classnames>[0])
-)
+type ClassnamesResolver<TagType, Additional> =
+  | TemplateStringsArray
+  | string
+  | ((
+      elProps: TClasserElProps<TagType, Additional>
+    ) => Parameters<typeof classnames>[0]);
 
-function makeClasser<TagType>(Tag: string) {
+function makeClasser<TagElementType>(Tag: string) {
+  return function classer<CustomProps>(
+    classnamesResolver: ClassnamesResolver<TagElementType, CustomProps>
+  ) {
+    return React.forwardRef<
+      TagElementType,
+      TClasserElProps<TagElementType, CustomProps>
+    >((passedProps, ref) => {
+      let resolvedClassname: string;
 
-    return function classer<Additional>(resolver: TClasserResolver<TagType, Additional>) {
+      if (typeof classnamesResolver === "function")
+        resolvedClassname = classnames(classnamesResolver(passedProps));
+      else if (typeof classnamesResolver === "string")
+        resolvedClassname = classnamesResolver;
+      else if (Array.isArray(classnamesResolver))
+        resolvedClassname = classnames(...classnamesResolver);
+      else resolvedClassname = "";
 
-        return React.forwardRef<TagType, TClasserElProps<TagType, Additional>>((passedProps, ref) => {
-
-            let resolvedClassname: string;
-
-            if (typeof resolver === "function")
-                resolvedClassname = classnames(resolver(passedProps));
-            else if (typeof resolver === "string")
-                resolvedClassname = resolver;
-            else if (Array.isArray(resolver))
-                resolvedClassname = classnames(...resolver);
-            else
-                //this should ~~theoretically~~ never happen with proper typescript usage
-                resolvedClassname = "";
-
-            return (
-                <Tag
-                    {...{
-                        ...passedProps,
-                        className: classnames(passedProps.className, resolvedClassname)
-                    }}
-                    ref={ref}
-                />
-            )
-        })
-    }
+      return (
+        <Tag
+          {...{
+            ...passedProps,
+            className: classnames(passedProps.className, resolvedClassname),
+          }}
+          ref={ref}
+        />
+      );
+    });
+  };
 }
-
 
 const containerClassers = {
-    div: makeClasser<HTMLDivElement>("div"),
-    article: makeClasser<HTMLDivElement>("article"),
-    section: makeClasser<HTMLDivElement>("section"),
-    span: makeClasser<HTMLSpanElement>("span"),
-    blockquote: makeClasser<HTMLQuoteElement>("blockquote"),
-    summary: makeClasser<HTMLSpanElement>("summary"),
-    pre: makeClasser<HTMLPreElement>("pre"),
+  div: makeClasser<HTMLDivElement>("div"),
+  article: makeClasser<HTMLDivElement>("article"),
+  section: makeClasser<HTMLDivElement>("section"),
+  span: makeClasser<HTMLSpanElement>("span"),
+  blockquote: makeClasser<HTMLQuoteElement>("blockquote"),
+  summary: makeClasser<HTMLSpanElement>("summary"),
+  pre: makeClasser<HTMLPreElement>("pre"),
 
-    table: makeClasser<HTMLTableElement>("table"),
-    tbody: makeClasser<HTMLTableSectionElement>("tbody"),
-    td: makeClasser<HTMLTableDataCellElement>("td"),
-    th: makeClasser<HTMLHeadElement>("th"),
-    tfoot: makeClasser<HTMLTableSectionElement>("tfoot"),
-
-}
+  table: makeClasser<HTMLTableElement>("table"),
+  tbody: makeClasser<HTMLTableSectionElement>("tbody"),
+  td: makeClasser<HTMLTableDataCellElement>("td"),
+  th: makeClasser<HTMLHeadElement>("th"),
+  tfoot: makeClasser<HTMLTableSectionElement>("tfoot"),
+};
 
 const miscClassers = {
-    image: makeClasser<HTMLImageElement>("image"),
-    img: makeClasser<HTMLImageElement>("img"),
-    video: makeClasser<HTMLVideoElement>("video"),
+  image: makeClasser<HTMLImageElement>("image"),
+  img: makeClasser<HTMLImageElement>("img"),
+  video: makeClasser<HTMLVideoElement>("video"),
 
-    a: makeClasser<HTMLLinkElement>("a"),
+  a: makeClasser<HTMLLinkElement>("a"),
 
-    b: makeClasser<HTMLSpanElement>("b"),
-    i: makeClasser<HTMLSpanElement>("i"),
+  b: makeClasser<HTMLSpanElement>("b"),
+  i: makeClasser<HTMLSpanElement>("i"),
 
-    svg: makeClasser<SVGElement>("svg"),
+  svg: makeClasser<SVGElement>("svg"),
 
-    strong: makeClasser<HTMLSpanElement>("strong"),
-    li: makeClasser<HTMLLIElement>("li"),
-    ul: makeClasser<HTMLUListElement>("ul"),
-    ol: makeClasser<HTMLOListElement>("ol"),
-    em: makeClasser<HTMLSpanElement>("em"),
+  strong: makeClasser<HTMLSpanElement>("strong"),
+  li: makeClasser<HTMLLIElement>("li"),
+  ul: makeClasser<HTMLUListElement>("ul"),
+  ol: makeClasser<HTMLOListElement>("ol"),
+  em: makeClasser<HTMLSpanElement>("em"),
 
-    form: makeClasser<HTMLFormElement>("form"),
-    textarea: makeClasser<HTMLTextAreaElement>("textarea"),
-    input: makeClasser<HTMLInputElement>("input"),
-    button: makeClasser<HTMLButtonElement>("button"),
-    label: makeClasser<HTMLLabelElement>("label"),
+  form: makeClasser<HTMLFormElement>("form"),
+  textarea: makeClasser<HTMLTextAreaElement>("textarea"),
+  input: makeClasser<HTMLInputElement>("input"),
+  button: makeClasser<HTMLButtonElement>("button"),
+  label: makeClasser<HTMLLabelElement>("label"),
+  select: makeClasser<HTMLSelectElement>("select"),
 
-    progress: makeClasser<HTMLProgressElement>("progress"),
-    noscript: makeClasser<HTMLElement>("noscript"),
+  progress: makeClasser<HTMLProgressElement>("progress"),
+  noscript: makeClasser<HTMLElement>("noscript"),
 };
 
 const headingClassers = {
-    h1: makeClasser<HTMLHeadingElement>("h1"),
-    h2: makeClasser<HTMLHeadingElement>("h2"),
-    h3: makeClasser<HTMLHeadingElement>("h3"),
-    h4: makeClasser<HTMLHeadingElement>("h4"),
-    h5: makeClasser<HTMLHeadingElement>("h5"),
-}
+  h1: makeClasser<HTMLHeadingElement>("h1"),
+  h2: makeClasser<HTMLHeadingElement>("h2"),
+  h3: makeClasser<HTMLHeadingElement>("h3"),
+  h4: makeClasser<HTMLHeadingElement>("h4"),
+  h5: makeClasser<HTMLHeadingElement>("h5"),
+};
 
 const classed = {
-    ...containerClassers,
-    ...miscClassers,
-    ...headingClassers,
-}
-
+  ...containerClassers,
+  ...miscClassers,
+  ...headingClassers,
+};
 
 export default classed;
